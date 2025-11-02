9db5187a6b38c5c5110eb208b4d0ff98d5d08947
Yongkang H. <149128930+hyongkang@users.noreply.github.com>
2025-11-02 16:33:16 -0600

    Update Terraform script with API Gateway REST APIs with OPTIONS methods

--- backend/infrastructure/main.tf
+++ backend/infrastructure/main.tf
@@ -100,3 +100,167 @@ resource "aws_lambda_function" "create_session_function" {
 
   runtime = "nodejs22.x"
 }
+
+resource "aws_api_gateway_rest_api" "session_api" {
+  name        = "session_api"
+  description = "API Gateway REST API for Bedrock functions"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# / OPTIONS
+resource "aws_api_gateway_method" "root_method" {
+  rest_api_id   = aws_api_gateway_rest_api.session_api.id
+  resource_id   = aws_api_gateway_rest_api.session_api.root_resource_id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "root_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_rest_api.session_api.root_resource_id
+  http_method = aws_api_gateway_method.root_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "root_integration" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_rest_api.session_api.root_resource_id
+  http_method = aws_api_gateway_method.root_method.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "root_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_rest_api.session_api.root_resource_id
+  http_method = aws_api_gateway_method.root_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_method.root_method, aws_api_gateway_integration.root_integration]
+}
+
+# /session
+resource "aws_api_gateway_resource" "session_resource" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  parent_id   = aws_api_gateway_rest_api.session_api.root_resource_id
+  path_part   = "session"
+}
+
+# OPTIONS for /session
+resource "aws_api_gateway_method" "session_method_options" {
+  rest_api_id   = aws_api_gateway_rest_api.session_api.id
+  resource_id   = aws_api_gateway_resource.session_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "session_method_response_options" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_resource.session_resource.id
+  http_method = aws_api_gateway_method.session_method_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "session_integration_options" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_resource.session_resource.id
+  http_method = aws_api_gateway_method.session_method_options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "session_integration_response_options" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_resource.session_resource.id
+  http_method = aws_api_gateway_method.session_method_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_method.session_method_options, aws_api_gateway_integration.session_integration_options]
+}
+
+#/session/{sessionId}
+resource "aws_api_gateway_resource" "session_id_resource" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  parent_id   = aws_api_gateway_resource.session_resource.id
+  path_part   = "{sessionId}"
+}
+
+# OPTIONS for /session/{sessionId}
+resource "aws_api_gateway_method" "session_id_method_options" {
+  rest_api_id   = aws_api_gateway_rest_api.session_api.id
+  resource_id   = aws_api_gateway_resource.session_id_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method_response" "session_id_method_response_options" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_resource.session_id_resource.id
+  http_method = aws_api_gateway_method.session_id_method_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+resource "aws_api_gateway_integration" "session_id_integration_options" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_resource.session_id_resource.id
+  http_method = aws_api_gateway_method.session_id_method_options.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "session_id_integration_response_options" {
+  rest_api_id = aws_api_gateway_rest_api.session_api.id
+  resource_id = aws_api_gateway_resource.session_id_resource.id
+  http_method = aws_api_gateway_method.session_id_method_options.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_method.session_id_method_options, aws_api_gateway_integration.session_id_integration_options]
+}
